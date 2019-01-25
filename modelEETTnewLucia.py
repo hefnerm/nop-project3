a213182b1be18201b9413e78ed183b9c2d6081c5
Gruppe 1 Netzwerkoptimierung in der Praxis <gruppe1@linux21>
2019-01-25 09:24:57 +0100

    neue loesung

--- modelEETTnewLucia.py
+++ modelEETTnewLucia.py
@@ -31,7 +31,7 @@ def solve_EETT(trainDic, powerDic, T_m, PL, ST, PassConOrd, timeHorizonMin, inst
 		model.Params.mipGap=0.000001
 	if (instance in [1,7,9,10]):
 		model.Params.timelimit=60*60*5
-		model.Params.mipGap=0.01
+#model.Params.mipGap=0.01
 
 	model.modelSense = GRB.MINIMIZE
 
